e0f2a8c8a05ce231152f3fad9269ee40b1d1dcd0
Aymen Awad <aymenawad2002amy@gmail.com>
2022-08-16 12:50:15 +0200

    ////

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,7 @@ namespace WebAPIBank
             });
 
             services.AddScoped<IKic, KicRepository>();
-
+            //omda
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
